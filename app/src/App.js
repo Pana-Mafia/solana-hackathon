8b5174a8b035e59dbd97bfcf389b00b5b249f92c
local user <36323477+ystgs@users.noreply.github.com>
2022-03-19 20:09:35 +0900

    一旦

--- app/src/App.js
+++ app/src/App.js
@@ -2,7 +2,7 @@ import './App.css';
 import React from 'react';
 // import { Connection } from '@solana/web3.js';
 // import { Provider } from '@project-serum/anchor';
-// import idl from './idl.json';
+import idl from './idl.json';
 import {
   getPhantomWallet,
 } from '@solana/wallet-adapter-wallets';
@@ -26,21 +26,153 @@ const wallets = [
 // const opts = {
 //   preflightCommitment: "processed"
 // }
-// const programID = new PublicKey(idl.metadata.address);
+const programID = new PublicKey(idl.metadata.address);
 function App() {
   // const [value] = useState(null);
   // const isConnectedToWallet = value;
   const wallet = useWallet();
-  // async function getProvider() {
-  //   /* create the provider and return it to the caller */
-  //   /* network set to local network for now */
-  //   const network = "http://127.0.0.1:8899";
-  //   const connection = new Connection(network, opts.preflightCommitment);
-  //   const provider = new Provider(
-  //     connection, wallet, opts.preflightCommitment,
-  //   );
-  //   return provider;
-  // }
+  async function getProvider() {
+    /* create the provider and return it to the caller */
+    /* network set to local network for now */
+    const network = "http://127.0.0.1:8899";
+    const connection = new Connection(network, opts.preflightCommitment);
+    const provider = new Provider(
+      connection, wallet, opts.preflightCommitment,
+    );
+    return provider;
+  }
+
+  const anchor = require("@project-serum/anchor");
+  const assert = require("assert");
+  const serumCmn = require("@project-serum/common");
+  const TokenInstructions = require("@project-serum/serum").TokenInstructions;
+
+  const TOKEN_PROGRAM_ID = new anchor.web3.PublicKey(
+    TokenInstructions.TOKEN_PROGRAM_ID.toString()
+  );
+
+  async function getTokenAccount(provider, addr) {
+    return await serumCmn.getTokenAccount(provider, addr);
+  }
+
+  async function getMintInfo(provider, mintAddr) {
+    return await serumCmn.getMintInfo(provider, mintAddr);
+  }
+
+  async function createMint(provider, authority) {
+    if (authority === undefined) {
+      authority = provider.wallet.publicKey;
+    }
+    const mint = anchor.web3.Keypair.generate();
+    const instructions = await createMintInstructions(
+      provider,
+      authority,
+      mint.publicKey
+    );
+
+    const tx = new anchor.web3.Transaction();
+    tx.add(...instructions);
+
+    await provider.send(tx, [mint]);
+
+    return mint.publicKey;
+  }
+
+  async function createMintInstructions(provider, authority, mint) {
+    let instructions = [
+      anchor.web3.SystemProgram.createAccount({
+        fromPubkey: provider.wallet.publicKey,
+        newAccountPubkey: mint,
+        space: 82,
+        lamports: await provider.connection.getMinimumBalanceForRentExemption(82),
+        programId: TOKEN_PROGRAM_ID,
+      }),
+      TokenInstructions.initializeMint({
+        mint,
+        decimals: 0,
+        mintAuthority: authority,
+      }),
+    ];
+    return instructions;
+  }
+
+  async function createTokenAccount(provider, mint, owner) {
+    const vault = anchor.web3.Keypair.generate();
+    const tx = new anchor.web3.Transaction();
+    tx.add(
+      ...(await createTokenAccountInstrs(provider, vault.publicKey, mint, owner))
+    );
+    await provider.send(tx, [vault]);
+    return vault.publicKey;
+  }
+
+  async function createTokenAccountInstrs(
+    provider,
+    newAccountPubkey,
+    mint,
+    owner,
+    lamports
+  ) {
+    if (lamports === undefined) {
+      lamports = await provider.connection.getMinimumBalanceForRentExemption(165);
+    }
+    return [
+      anchor.web3.SystemProgram.createAccount({
+        fromPubkey: provider.wallet.publicKey,
+        newAccountPubkey,
+        space: 165,
+        lamports,
+        programId: TOKEN_PROGRAM_ID,
+      }),
+      TokenInstructions.initializeAccount({
+        account: newAccountPubkey,
+        mint,
+        owner,
+      }),
+    ];
+  }
+
+  // 処理追加終わり
+  async function mintNewToken() {
+    console.log("mintNewToken is called.")
+
+    const anchor = require("@project-serum/anchor");
+    const assert = require("assert");
+
+    const serumCmn = require("@project-serum/common");
+    const TokenInstructions = require("@project-serum/serum").TokenInstructions;
+
+    let mint = 1;
+    let from = '2junxGTVZLmitbrS7qNHWLgPe9QLsApYaujsUFwsj5ym';
+    let to = '2junxGTVZLmitbrS7qNHWLgPe9QLsApYaujsUFwsj5ym';
+
+
+    const provider = await getProvider()
+    console.log()
+    const program = new Program(idl, programID, provider);
+
+    // mint = await createMint(provider);
+    // from = await createTokenAccount(provider, mint, provider.wallet.publicKey);
+    // to = await createTokenAccount(provider, mint, provider.wallet.publicKey);
+
+    try {
+      await program.rpc.proxyMintTo(new anchor.BN(1000), {
+        accounts: {
+          authority: provider.wallet.publicKey,
+          mint,
+          to: provider.wallet.publicKey,
+          tokenProgram: TokenInstructions.TOKEN_PROGRAM_ID,
+        },
+      })
+
+      const fromAccount = await getTokenAccount(provider, from);
+      assert.ok(fromAccount.amount.eq(new anchor.BN(1000)));
+
+    } catch (err) {
+      console.error(err)
+    }
+  }
+
   if (!wallet.connected) {
     /* If the user's wallet is not connected, display connect wallet button. */
     return (
